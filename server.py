7fbff06e177cfc5348c59a11ff069b5011bfc277
TTC71 <pvesaphong@gmail.com>
2025-04-30 14:29:13 +0200

    Update server.py

--- server.py
+++ server.py
@@ -39,3 +39,6 @@ def importer():
         return jsonify({"message": "Import CSV terminé avec succès."})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
